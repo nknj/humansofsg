8f7eee65781fdb9de38115f724a015ec565726b9
Nikunj Handa <nikunj.sg@gmail.com>
2013-06-23 21:16:26 +0800

    Configured settings (for heroku and local)

---
+++ humansofsg/settings/local.py
@@ -0,0 +1,13 @@
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'humansofsg',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
