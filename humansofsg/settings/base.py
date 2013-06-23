aa460f5d912d80bdab57a8ca0eab490527802d38
Nikunj Handa <nikunj.sg@gmail.com>
2013-06-23 23:12:04 +0800

    corrected static files url

--- humansofsg/settings/base.py
+++ humansofsg/settings/base.py
@@ -65,7 +65,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = 'root("..", "static")'
+STATIC_ROOT = root("..", "static")
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
